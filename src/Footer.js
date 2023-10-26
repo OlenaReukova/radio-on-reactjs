7a64a5b90b26ffd051b1bc9dd695a33c928cd0bf
Olena Reukova <kronverk01@gmail.com>
2023-10-26 19:48:26 +0100

    front-end

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className='footer'>
+      <div className='footer-left'>
+        <p> 2023</p>
+      </div>
+      <div className='footer-right'></div>
+    </footer>
+  );
+}
+
+export default Footer;
